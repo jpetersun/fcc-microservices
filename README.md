d5499d988c3ef557264ba7242a8a26f74fb31298
James Peterson <petersonj020@gmail.com>
2018-08-24 11:50:51 -0700

    chore: update homepage and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fcc-microservices
+
+freeCodeCamp APIs & Microservices projects built with Node.js, Express and hosted on <a href="https://glitch.com" target="_blank">glitch.com</a>
--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 const express = require('express')
-const boom = require('boom')
 
 const app = express()
 const PORT = process.env.PORT || 8000
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`)
 })
 
-// error handler middleware using boom
-app.use((err, req, res, next) => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log(err)
-  }
-
-  return res.status(err.output.statusCode).json(err.output.payload)
-})
-
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
-
-module.exports = app
